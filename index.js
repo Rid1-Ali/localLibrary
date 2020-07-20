372e97434352ad41b38c13cbd2d61a5ad43e2126
Ridwan A. Mohammed <redwan2299@gmail.com>
2020-07-21 00:52:48 +0300

    pug started

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 "use strict";
 
 var express = require('express')
+const path = require('path');
 var app = express();
+
 
 var indexRouter = require('./router/index');
 var usersRouter = require('./router/users');
 var catalogRouter = require('./router/catalog');
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 app.use('/', indexRouter)
 //app.use('/users', usersRouter)
 app.use('/catalog', catalogRouter)
+app.use('/test', function (req, res) {
+    res.render('layout')
+})
 
 
 
